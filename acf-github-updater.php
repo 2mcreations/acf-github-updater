c381d60fa5b473c543a448b0bb7277fdf9f0bf90
Mattia Calore <43504787+2mcreations@users.noreply.github.com>
2026-04-03 08:35:12 +0000

    tag on zip

--- acf-github-updater.php
+++ acf-github-updater.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ACF Pro GitHub Updater
  * Description: Aggiorna Advanced Custom Fields PRO dalle release GitHub
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: 2mcreations.it
  * Author URI: https://2mcreations.it
  */
